f143968d465f1aa33b2a618032463661e720da1f
destroy-man <73271630+destroy-man@users.noreply.github.com>
2020-10-23 11:27:16 +0500

    Add files via upload

---
+++ chat/client/Main.java
@@ -0,0 +1,11 @@
+package client;
+
+/**
+ *
+ * @author Коробейников Константин
+ */
+public class Main {
+    public static void main(String[] args) {
+        ClientWindow clientWindow=new ClientWindow();
+    }   
+}
---
+++ chat/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ *
+ * @author Коробейников Константин
+ */
+public class Main {
+    public static void main(String[] args){
+        Server server=new Server();
+    }
+}
