1ec6240ca32769d9e46f22076e1ced02d3c813c2
destroy-man <73271630+destroy-man@users.noreply.github.com>
2020-11-02 15:49:10 +0500

    Add files via upload

---
+++ Android applications/p0061layouts/java/com/korobeynikov/p0061layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.korobeynikov.p0061layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alayout);
+    }
+}
