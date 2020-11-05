090769aa2ee82604174a623914668b6a1c9df760
destroy-man <73271630+destroy-man@users.noreply.github.com>
2020-11-05 14:05:42 +0500

    Add files via upload

---
+++ Android applications/p0111resvalues/java/com/korobeynikov/p0111resvalues/MainActivity.java
@@ -0,0 +1,23 @@
+package com.korobeynikov.p0111resvalues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout llBottom=findViewById(R.id.llBottom);
+        TextView tvBottom=findViewById(R.id.tvBottom);
+        Button btnBottom=findViewById(R.id.btnBottom);
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+    }
+}
