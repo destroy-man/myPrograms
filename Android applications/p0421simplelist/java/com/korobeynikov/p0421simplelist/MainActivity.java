c69eeab15862e4368283b0668cad42cd87d3d9b6
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-01-12 13:01:05 +0500

    Add files via upload

---
+++ Android applications/p0421simplelist/java/com/korobeynikov/p0421simplelist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.korobeynikov.p0421simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names={"Иван","Марья","Петр","Антон","Даша","Борис","Костя","Игорь","Анна","Денис","Андрей"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lvMain=findViewById(R.id.lvMain);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.my_list_item,names);
+        lvMain.setAdapter(adapter);
+    }
+}
