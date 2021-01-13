b5bd50c391ad4d4b61e5bb30cb61655564cb38bb
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-01-13 13:12:22 +0500

    Add files via upload

---
+++ Android applications/p0431simplelistchoice/java/com/korobeynikov/p0431simplelistchoice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.korobeynikov.p0431simplelistchoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    final String LOG_TAG="myLogs";
+    ListView lvMain;
+    String[] names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvMain=findViewById(R.id.lvMain);
+        lvMain.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.names, android.R.layout.simple_list_item_multiple_choice);
+        lvMain.setAdapter(adapter);
+        Button btnChecked=findViewById(R.id.btnChecked);
+        btnChecked.setOnClickListener(this);
+        names=getResources().getStringArray(R.array.names);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(LOG_TAG,"checked: ");
+        SparseBooleanArray sbArray=lvMain.getCheckedItemPositions();
+        for(int i=0;i<sbArray.size();i++){
+            int key=sbArray.keyAt(i);
+            if(sbArray.get(key))
+                Log.d(LOG_TAG,names[key]);
+        }
+    }
+}
