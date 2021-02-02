642c8dc951b1debbb161612d8fcef4067a4a9ba1
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-02-02 18:10:25 +0500

    Add files via upload

---
+++ Android/RssNews/java/com/korobeynikov/rssnews/RssItem.java
@@ -0,0 +1,144 @@
+package com.korobeynikov.rssnews;
+import android.os.AsyncTask;
+
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+public class RssItem {
+    private String title;
+    private String description;
+    private Date pubDate;
+    private String link;
+
+    static ArrayList<RssItem> rssItems;
+    static boolean hasRss=false;
+
+    public RssItem(String title, String description, Date pubDate, String link) {
+        this.title = title;
+        this.description = description;
+        this.pubDate = pubDate;
+        this.link = link;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getLink()
+    {
+        return this.link;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public Date getPubDate()
+    {
+        return this.pubDate;
+    }
+
+    @Override
+    public String toString() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd - hh:mm:ss");
+
+        String result = getTitle() + "  ( " + sdf.format(this.getPubDate()) + " )";
+        return result;
+    }
+
+    static class GroupConnection extends AsyncTask< String, Void, String > {
+
+        @Override
+        protected String doInBackground(String... args) {
+            URL url;
+            HttpURLConnection conn=null;
+            try {
+                //open an URL connection make GET to the server and
+                //take xml RSS data
+                url = new URL(args[0]);
+                conn = (HttpURLConnection) url.openConnection();
+
+                conn.setRequestMethod("GET");
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(5000);
+                conn.connect();
+
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStream is = conn.getInputStream();
+
+                    //DocumentBuilderFactory, DocumentBuilder are used for
+                    //xml parsing
+                    DocumentBuilderFactory dbf = DocumentBuilderFactory
+                            .newInstance();
+                    DocumentBuilder db = dbf.newDocumentBuilder();
+
+                    //using db (Document Builder) parse xml data and assign
+                    //it to Element
+                    Document document = db.parse(is);
+                    Element element = document.getDocumentElement();
+
+                    //take rss nodes to NodeList
+                    NodeList nodeList = element.getElementsByTagName("item");
+
+                    if (nodeList.getLength() > 0) {
+                        for (int i = 0; i < nodeList.getLength(); i++) {
+
+                            //take each entry (corresponds to <item></item> tags in
+                            //xml data
+
+                            Element entry = (Element) nodeList.item(i);
+
+                            Element _titleE = (Element) entry.getElementsByTagName("title").item(0);
+                            Element _descriptionE = (Element) entry.getElementsByTagName("description").item(0);
+                            Element _pubDateE = (Element) entry.getElementsByTagName("pubDate").item(0);
+                            Element _linkE = (Element) entry.getElementsByTagName("link").item(0);
+
+                            String _title = _titleE.getFirstChild().getNodeValue();
+                            String _description = _descriptionE.getFirstChild().getNodeValue();
+                            Date _pubDate = new Date(_pubDateE.getFirstChild().getNodeValue());
+                            String _link = _linkE.getFirstChild().getNodeValue();
+
+                            //create RssItemObject and add it to the ArrayList
+                            RssItem rssItem = new RssItem(_title, _description,
+                                                          _pubDate, _link);
+
+                            rssItems.add(rssItem);
+                            if(rssItems.size()==10)
+                                break;
+                        }
+                    }
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(conn!=null)
+                    conn.disconnect();
+            }
+            hasRss=true;
+            return null;
+        }
+    }
+
+    public static ArrayList<RssItem> getRssItems(String feedUrl) {
+
+        rssItems = new ArrayList<RssItem>();
+        try {
+            hasRss=false;
+            new GroupConnection().execute(feedUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while(!hasRss){
+
+        }
+        return rssItems;
+    }
+}
---
+++ Android/RssNews/java/com/korobeynikov/rssnews/RssItemDisplayer.java
@@ -0,0 +1,39 @@
+package com.korobeynikov.rssnews;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import android.view.*;
+public class RssItemDisplayer extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rss_item_displayer);
+
+        RssItem selectedRssItem = MainActivity.selectedRssItem;
+
+        WebView wb=findViewById(R.id.wbRss);
+        wb.loadUrl(selectedRssItem.getLink());
+        //Bundle extras = getIntent().getExtras();
+//        TextView titleTv = (TextView)findViewById(R.id.titleTextView);
+//        TextView contentTv = (TextView)findViewById(R.id.contentTextView);
+//
+//        String title = "";
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd - hh:mm:ss");
+//        title = "\n" + selectedRssItem.getTitle() + "  ( "
+//                + sdf.format(selectedRssItem.getPubDate()) + " )\n\n";
+//
+//        String content = "";
+//        content += selectedRssItem.getDescription() + "\n"
+//                + selectedRssItem.getLink();
+//
+//        titleTv.setText(title);
+//        contentTv.setText(content);
+    }
+}
