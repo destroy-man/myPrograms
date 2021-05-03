151d55336cd13a6e1fbbb4e3d336876240d4b4d5
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-05-03 13:14:39 +0500

    Add files via upload

---
+++ AndroidLessons/RxJava/p0091retrofit2retrolambda/java/com/korobeynikov/p0091retrofit2retrolambda/WebApi.java
@@ -0,0 +1,14 @@
+package com.korobeynikov.p0091retrofit2retrolambda;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface WebApi {
+
+    @GET("messages{page}.json")
+    Observable<List<Message>> messages(@Path("page") int page);
+}
