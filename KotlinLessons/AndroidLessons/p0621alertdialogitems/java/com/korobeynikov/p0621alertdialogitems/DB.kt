7b2df0f2c97148c1323b69641505b61fe5da7435
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-06-20 13:09:59 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0621alertdialogitems/java/com/korobeynikov/p0621alertdialogitems/DB.kt
@@ -0,0 +1,63 @@
+package com.korobeynikov.p0621alertdialogitems
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DB(ctx:Context){
+
+    private val DB_NAME="mydb"
+    private val DB_VERSION=1
+    private val DB_TABLE="mytab"
+
+    companion object{
+        val COLUMN_ID="_id"
+        val COLUMN_TXT="txt"
+    }
+
+    private val DB_CREATE="create table "+DB_TABLE+"("+COLUMN_ID+" integer primary key, "+COLUMN_TXT+" text);"
+    private lateinit var mDBHelper:DBHelper
+    private lateinit var mDB:SQLiteDatabase
+    private val mCtx=ctx
+
+    fun open(){
+        mDBHelper=DBHelper(mCtx,DB_NAME,null,DB_VERSION)
+        mDB=mDBHelper.writableDatabase
+    }
+
+    fun close(){
+        if(mDBHelper!=null)
+            mDBHelper.close()
+    }
+
+    fun getAllData():Cursor{
+        return mDB.query(DB_TABLE,null,null,null,null,null,null)
+    }
+
+    fun changeRec(id:Int,txt:String){
+        val cv=ContentValues()
+        cv.put(COLUMN_TXT,txt)
+        mDB.update(DB_TABLE,cv,COLUMN_ID+" = "+id,null)
+    }
+
+    private inner class DBHelper:SQLiteOpenHelper{
+
+        constructor(context:Context,name:String,factory:SQLiteDatabase.CursorFactory?,version:Int):
+                super(context,name,factory,version)
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            db?.execSQL(DB_CREATE)
+            val cv=ContentValues()
+            for(i in 1..4){
+                cv.put(COLUMN_ID,i)
+                cv.put(COLUMN_TXT,"sometext "+i)
+                db?.insert(DB_TABLE,null,cv)
+            }
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+
+    }
+}
---
+++ KotlinLessons/AndroidLessons/p0621alertdialogitems/java/com/korobeynikov/p0621alertdialogitems/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.korobeynikov.p0621alertdialogitems
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    val LOG_TAG="myLogs"
+    val DIALOG_ITEMS=1
+    val DIALOG_ADAPTER=2
+    val DIALOG_CURSOR=3
+    var cnt=0
+    lateinit var db:DB
+    lateinit var cursor:Cursor
+    val data=arrayOf("one","two","three","four")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        db=DB(this)
+        db.open()
+        cursor=db.getAllData()
+        startManagingCursor(cursor)
+    }
+
+    fun onclick(v:View){
+        changeCount()
+        when(v.id){
+            R.id.btnItems->showDialog(DIALOG_ITEMS)
+            R.id.btnAdapter->showDialog(DIALOG_ADAPTER)
+            R.id.btnCursor->showDialog(DIALOG_CURSOR)
+        }
+    }
+
+    protected override fun onCreateDialog(id:Int):Dialog{
+        val adb=AlertDialog.Builder(this)
+        when(id){
+            DIALOG_ITEMS->{
+                adb.setTitle(R.string.items)
+                adb.setItems(data,myClickListener)
+            }
+            DIALOG_ADAPTER->{
+                adb.setTitle(R.string.adapter)
+                val adapter=ArrayAdapter<String>(this,android.R.layout.select_dialog_item,data)
+                adb.setAdapter(adapter,myClickListener)
+            }
+            DIALOG_CURSOR->{
+                adb.setTitle(R.string.cursor)
+                adb.setCursor(cursor,myClickListener,DB.COLUMN_TXT)
+            }
+        }
+        return adb.create()
+    }
+
+    protected override fun onPrepareDialog(id:Int,dialog:Dialog){
+        val aDialog=dialog as AlertDialog
+        val lAdapter=aDialog.listView.adapter
+        when(id){
+            DIALOG_ITEMS,DIALOG_ADAPTER->{
+                if(lAdapter is BaseAdapter){
+                    val bAdapter=lAdapter as BaseAdapter
+                    bAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    val myClickListener=DialogInterface.OnClickListener{dialog,which->
+        Log.d(LOG_TAG,"which = "+which)
+    }
+
+    fun changeCount(){
+        cnt++
+        data[3]=""+cnt
+        db.changeRec(4,""+cnt)
+        cursor.requery()
+    }
+
+    protected override fun onDestroy(){
+        super.onDestroy()
+        db.close()
+    }
+}
