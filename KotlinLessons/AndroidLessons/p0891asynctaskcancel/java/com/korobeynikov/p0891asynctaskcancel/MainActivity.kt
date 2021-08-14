da6f55166dfdf551c8111132ef4634b05f2b0637
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-08-14 13:10:40 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0891asynctaskcancel/java/com/korobeynikov/p0891asynctaskcancel/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.korobeynikov.p0891asynctaskcancel
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    val LOG_TAG="myLogs"
+    lateinit var mt:MyTask
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onclick(v:View){
+        when(v.id){
+            R.id.btnStart->{
+                mt=MyTask()
+                mt.execute()
+            }
+            R.id.btnCancel->cancelTask()
+        }
+    }
+
+    fun cancelTask(){
+        if(mt==null)return
+        Log.d(LOG_TAG,"Cancel result: "+mt.cancel(true))
+    }
+
+    inner class MyTask:AsyncTask<Unit,Unit,Unit>(){
+
+        protected override fun onPreExecute() {
+            super.onPreExecute()
+            tvInfo.text="Begin"
+            Log.d(LOG_TAG,"Begin")
+        }
+
+        protected override fun doInBackground(vararg params: Unit?): Unit? {
+            try{
+                for(i in 0..4){
+                    TimeUnit.SECONDS.sleep(1)
+                    Log.d(LOG_TAG,"isCancelled: "+isCancelled)
+                }
+            }
+            catch(e:InterruptedException){
+                Log.d(LOG_TAG,"Interrupted")
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        protected override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            tvInfo.text="End"
+            Log.d(LOG_TAG,"End")
+        }
+
+        protected override fun onCancelled() {
+            super.onCancelled()
+            tvInfo.text="Cancel"
+            Log.d(LOG_TAG,"Cancel")
+        }
+    }
+}
