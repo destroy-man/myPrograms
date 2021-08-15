2de563b8ffc1c71705c106e90bfd1ccedc30ea74
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-08-15 13:06:50 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0901asynctaskstatus/java/com/korobeynikov/p0901asynctaskstatus/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.korobeynikov.p0901asynctaskstatus
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mt:MyTask
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onclick(v:View){
+        when(v.id){
+            R.id.btnStart->startTask()
+            R.id.btnStatus->showStatus()
+        }
+    }
+
+    private fun startTask(){
+        mt=MyTask()
+        mt.execute()
+        mt.cancel(false)
+    }
+
+    private fun showStatus(){
+        if(mt!=null)
+            if(mt.isCancelled)
+                Toast.makeText(this,"CANCELLED",Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this,mt.status.toString(),Toast.LENGTH_SHORT).show()
+    }
+
+    inner class MyTask:AsyncTask<Unit,Unit,Unit>(){
+
+        protected override fun onPreExecute() {
+            super.onPreExecute()
+            tvInfo.text="Begin"
+        }
+
+        protected override fun doInBackground(vararg params: Unit?): Unit? {
+            try{
+                for(i in 0..4){
+                    if(isCancelled)return null
+                    TimeUnit.SECONDS.sleep(1)
+                }
+            }
+            catch(e:InterruptedException){
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        protected override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            tvInfo.text="End"
+        }
+
+        protected override fun onCancelled() {
+            super.onCancelled()
+            tvInfo.text="Cancel"
+        }
+    }
+}
