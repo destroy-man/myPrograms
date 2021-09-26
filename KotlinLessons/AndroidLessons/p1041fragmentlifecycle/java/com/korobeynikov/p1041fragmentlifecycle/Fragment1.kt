dbcf7fd0934702e05672ce249ff47571c8b39eee
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-09-26 13:11:43 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1041fragmentlifecycle/java/com/korobeynikov/p1041fragmentlifecycle/Fragment1.kt
@@ -0,0 +1,70 @@
+package com.korobeynikov.p1041fragmentlifecycle
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1: Fragment() {
+
+    val LOG_TAG="myLogs"
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        Log.d(LOG_TAG,"Fragment1 onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(LOG_TAG,"Fragment1 onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d(LOG_TAG,"Fragment1 onCreateView")
+        return inflater.inflate(R.layout.fragment1,null)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.d(LOG_TAG,"Fragment1 onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG,"Fragment1 onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG,"Fragment1 onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG,"Fragment1 onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG,"Fragment1 onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(LOG_TAG,"Fragment1 onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG,"Fragment1 onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(LOG_TAG,"Fragment1 onDetach")
+    }
+}
