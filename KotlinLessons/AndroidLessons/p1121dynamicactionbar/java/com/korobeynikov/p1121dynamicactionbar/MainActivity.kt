037c8a1a01e8051401d49dc30711e204981458de
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-10-16 13:21:24 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1121dynamicactionbar/java/com/korobeynikov/p1121dynamicactionbar/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.korobeynikov.p1121dynamicactionbar
+
+import android.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.CheckBox
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val MENU_ID=1
+    lateinit var frag1:Fragment
+    lateinit var frag2:Fragment
+    lateinit var frag:Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        frag1=Fragment1()
+        frag=frag1
+        frag2=Fragment2()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        menu?.setGroupVisible(R.id.groupVsbl,chbVisible.isChecked)
+        if(chbAddDel.isChecked)
+            menu?.add(0,MENU_ID,0,R.string.menu_item1)?.setIcon(android.R.drawable.ic_delete)
+                ?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS or MenuItem.SHOW_AS_ACTION_WITH_TEXT)
+        else
+            menu?.removeItem(MENU_ID)
+        return true
+    }
+
+    fun onClick(view:View){
+        when(view.id){
+            R.id.chbAddDel,R.id.chbVisible->invalidateOptionsMenu()
+            R.id.btnFrag->{
+                frag=if(frag==frag1)frag2 else frag1
+                fragmentManager.beginTransaction().replace(R.id.cont,frag).commit()
+            }
+        }
+    }
+}
