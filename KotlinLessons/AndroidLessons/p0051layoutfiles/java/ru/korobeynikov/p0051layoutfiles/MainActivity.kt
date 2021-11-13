7cb529653437027911a7b160d8381d543ce335ea
destroy-man <73271630+destroy-man@users.noreply.github.com>
2021-11-13 13:05:42 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0051layoutfiles/java/ru/korobeynikov/p0051layoutfiles/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.korobeynikov.p0051layoutfiles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.myscreen)
+    }
+}
