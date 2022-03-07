41c745c04fbc2dbdf99bef6454fd21660efeb672
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-03-07 13:06:45 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0541customadapter/java/ru/korobeynikov/p0541customadapter/Product.kt
@@ -0,0 +1,8 @@
+package ru.korobeynikov.p0541customadapter
+
+class Product(_describe:String,_price:Int,_image:Int,_box:Boolean) {
+    val name=_describe
+    val price=_price
+    val image=_image
+    var box=_box
+}
