665b1a35aadcf0826c07157c2a0dd86fb6de0cb3
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-04-16 13:34:39 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0771tabintent/java/ru/korobeynikov/p0771tabintent/OneActivity.kt
@@ -0,0 +1,11 @@
+package ru.korobeynikov.p0771tabintent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.one)
+    }
+}
