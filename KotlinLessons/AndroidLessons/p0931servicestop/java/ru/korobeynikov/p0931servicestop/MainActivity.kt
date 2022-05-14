d7a5483f1ab9de4cfc0da6c318a016b00622c16f
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-05-14 13:07:34 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p0931servicestop/java/ru/korobeynikov/p0931servicestop/MainActivity.kt
@@ -0,0 +1,20 @@
+package ru.korobeynikov.p0931servicestop
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickStart(v:View){
+        startService(Intent(this,MyService::class.java).putExtra("time",7))
+        startService(Intent(this,MyService::class.java).putExtra("time",2))
+        startService(Intent(this,MyService::class.java).putExtra("time",4))
+    }
+}
