e676e9156c1fedd97a3914c15ccac24abac49bfc
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-06-17 13:31:52 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1101dialogfragment/java/ru/korobeynikov/p1101dialogfragment/MainActivity.kt
@@ -0,0 +1,27 @@
+package ru.korobeynikov.p1101dialogfragment
+
+import android.app.DialogFragment
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var dlg1:DialogFragment
+    lateinit var dlg2:DialogFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dlg1=Dialog1()
+        dlg2=Dialog2()
+    }
+
+    fun onClick(v:View){
+        when(v.id){
+            R.id.btnDlg1->dlg1.show(fragmentManager,"dlg1")
+            R.id.btnDlg2->dlg2.show(fragmentManager,"dlg2")
+        }
+    }
+}
