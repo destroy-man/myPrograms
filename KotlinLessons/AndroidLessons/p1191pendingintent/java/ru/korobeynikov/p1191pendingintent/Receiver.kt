03ee7ad5d182e6222dce06857741ceecb593bfa3
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-06-24 15:20:48 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1191pendingintent/java/ru/korobeynikov/p1191pendingintent/Receiver.kt
@@ -0,0 +1,17 @@
+package ru.korobeynikov.p1191pendingintent
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class Receiver : BroadcastReceiver() {
+
+    val LOG_TAG="myLogs"
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(LOG_TAG,"onReceive")
+        Log.d(LOG_TAG,"action = ${intent.action}")
+        Log.d(LOG_TAG,"extra = ${intent.getStringExtra("extra")}")
+    }
+}
