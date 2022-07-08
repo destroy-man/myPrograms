5c258acf1fa5c3e177e49002213883a3d2c2f3d2
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-07-08 14:41:16 +0500

    Delete MyDictionaryComponent.kt

--- Kotlin/MyDictionary/main/java/ru/korobeynikov/mydictionary/MyDictionaryComponent.kt
+++
@@ -1,8 +0,0 @@
-package ru.korobeynikov.mydictionary
-
-import dagger.Component
-
-@Component(modules = [RealmConfigModule::class, CoroutineScopeModule::class])
-interface MyDictionaryComponent {
-    fun injectMainActivity(mainActivity: MainActivity)
-}
