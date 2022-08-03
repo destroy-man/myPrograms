b3b48944b1c48418f283fab4e535f5a16ba010ff
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-08-03 16:49:36 +0500

    Delete Java. Руководство для начинающих Шилдт/1 directory

--- Java. Руководство для начинающих Шилдт/1/ForDemo.java
+++
@@ -1,8 +0,0 @@
-class ForDemo{
-	public static void main(String[] args){
-		int count;
-		for(count=0;count<5;count++)
-			System.out.println("Значение счетчика: "+count);
-		System.out.println("Готово!");
-	}
-}
--- Java. Руководство для начинающих Шилдт/1/WeightOnMoon.java
+++
@@ -1,6 +0,0 @@
-//Глава 1 вопрос 9
-class WeightOnMoon{
-	public static void main(String[] args){
-		System.out.println("Мой вес на Луне "+(63*0.17));
-	}
-}
