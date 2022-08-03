1dff136201fe8ddfae4f6325ef7a927912666c8c
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-08-03 17:08:26 +0500

    Delete Spring/springapp1 directory

--- Spring/springapp1/src/main/java/springcourse/Music.java
+++
@@ -1,5 +0,0 @@
-package springcourse;
-
-public interface Music {
-    String getSong();
-}
--- Spring/springapp1/src/main/java/springcourse/RockMusic.java
+++
@@ -1,9 +0,0 @@
-package springcourse;
-
-public class RockMusic implements Music{
-
-    @Override
-    public String getSong() {
-        return "Wind cries Mary";
-    }
-}
