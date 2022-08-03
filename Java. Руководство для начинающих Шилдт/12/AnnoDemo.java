274caa5f585b6c8af850bfa31b33ca8a2aa4cd3c
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-08-03 16:50:19 +0500

    Delete Java. Руководство для начинающих Шилдт/12 directory

--- Java. Руководство для начинающих Шилдт/12/AnnoDemo.java
+++
@@ -1,6 +0,0 @@
-class AnnoDemo{
-	public static void main(String[] args){
-		MyClass myObj=new MyClass("тест");
-		System.out.println(myObj.getMsg());
-	}
-}
--- Java. Руководство для начинающих Шилдт/12/AutoBox.java
+++
@@ -1,7 +0,0 @@
-class AutoBox{
-	public static void main(String[] args){
-		Integer iOb=100;
-		int i=iOb;
-		System.out.println(i+" "+iOb);
-	}
-}
