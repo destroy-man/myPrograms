0068824db680069442d3c128aa579828cd74feed
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-09-15 16:55:57 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1821constraintlayoutcircular/java/ru/korobeynikov/p1821constraintlayoutcircular/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.korobeynikov.p1821constraintlayoutcircular
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        group.visibility = View.GONE
+    }
+}
