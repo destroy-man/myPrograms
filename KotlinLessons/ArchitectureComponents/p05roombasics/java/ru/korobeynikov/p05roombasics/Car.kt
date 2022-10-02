ea703167675141a6f92d210fc308f94430b4350c
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-10-02 08:04:21 +0000

    Add files via upload

---
+++ KotlinLessons/ArchitectureComponents/p05roombasics/java/ru/korobeynikov/p05roombasics/Car.kt
@@ -0,0 +1,12 @@
+package ru.korobeynikov.p05roombasics
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Car(
+    @PrimaryKey
+    val id: Long,
+    val model: String,
+    val year: Int,
+)
