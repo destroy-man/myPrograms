76df22a952b2ff321f2bce9cb2c51f6008227741
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-10-07 12:00:52 +0000

    Add files via upload

---
+++ KotlinLessons/ArchitectureComponents/p02livedata/java/ru/korobeynikov/p02livedata/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.korobeynikov.p02livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val liveData = DataController.getData()
+        liveData.observe(this) { value ->
+            textValue.text = value
+        }
+    }
+}
