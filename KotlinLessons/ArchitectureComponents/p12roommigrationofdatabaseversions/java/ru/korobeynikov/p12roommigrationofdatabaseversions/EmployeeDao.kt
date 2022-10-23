6b46a29c8fa3ee335a30a48f4aa47accd1c2e0c1
destroy-man <73271630+destroy-man@users.noreply.github.com>
2022-10-23 08:19:20 +0000

    Add files via upload

---
+++ KotlinLessons/ArchitectureComponents/p12roommigrationofdatabaseversions/java/ru/korobeynikov/p12roommigrationofdatabaseversions/EmployeeDao.kt
@@ -0,0 +1,22 @@
+package ru.korobeynikov.p12roommigrationofdatabaseversions
+
+import androidx.room.*
+
+@Dao
+interface EmployeeDao {
+
+    @Query("SELECT * FROM employee")
+    fun getAll(): List<Employee>
+
+    @Query("SELECT * FROM employee WHERE id = :id")
+    fun getById(id: Long): Employee
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(employee: Employee)
+
+    @Update
+    fun update(employee: List<Employee>): Int
+
+    @Delete
+    fun delete(employee: List<Employee>): Int
+}
