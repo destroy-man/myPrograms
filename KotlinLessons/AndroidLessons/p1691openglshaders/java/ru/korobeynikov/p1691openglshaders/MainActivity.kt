3c037d93578942bcd57c1463018ed8f2fde95326
destroy-man <73271630+destroy-man@users.noreply.github.com>
2023-07-09 13:04:27 +0500

    Add files via upload

---
+++ KotlinLessons/AndroidLessons/p1691openglshaders/java/ru/korobeynikov/p1691openglshaders/MainActivity.kt
@@ -0,0 +1,42 @@
+package ru.korobeynikov.p1691openglshaders
+
+import android.app.ActivityManager
+import android.opengl.GLSurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var glSurfaceView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!supportES32()) {
+            Toast.makeText(this, "OpenGl ES 3.2 is not supported", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+        glSurfaceView = GLSurfaceView(this)
+        glSurfaceView.setEGLContextClientVersion(3)
+        glSurfaceView.setRenderer(OpenGLRenderer(this))
+        setContentView(glSurfaceView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        glSurfaceView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glSurfaceView.onResume()
+    }
+
+    private fun supportES32(): Boolean {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val configurationInfo = activityManager.deviceConfigurationInfo
+        return configurationInfo.reqGlEsVersion >= 0x30002
+    }
+}
---
+++ KotlinLessons/AndroidLessons/p1691openglshaders/java/ru/korobeynikov/p1691openglshaders/OpenGLRenderer.kt
@@ -0,0 +1,52 @@
+package ru.korobeynikov.p1691openglshaders
+
+import android.content.Context
+import android.opengl.GLES32.*
+import android.opengl.GLSurfaceView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class OpenGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
+
+    private var programId = 0
+    private var uColorLocation = 0
+    private var aPositionLocation = 0
+    private val vertexData: FloatBuffer
+
+    init {
+        val vertices = floatArrayOf(-0.5f, -0.2f, 0f, 0.2f, 0.5f, -0.2f)
+        vertexData = ByteBuffer.allocateDirect(vertices.size * 4)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer()
+        vertexData.put(vertices)
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        glClearColor(0f, 0f, 0f, 1f)
+        val vertexShaderId = ShaderUtils.createShader(context, GL_VERTEX_SHADER, R.raw.vertex_shader)
+        val fragmentShaderId = ShaderUtils.createShader(context, GL_FRAGMENT_SHADER, R.raw.fragment_shader)
+        programId = ShaderUtils.createProgram(vertexShaderId, fragmentShaderId)
+        glUseProgram(programId)
+        bindData()
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        glViewport(0, 0, width, height)
+    }
+
+    private fun bindData() {
+        uColorLocation = glGetUniformLocation(programId, "u_Color")
+        glUniform4f(uColorLocation, 0f, 0f, 1f, 1f)
+        aPositionLocation = glGetAttribLocation(programId, "a_Position")
+        vertexData.position(0)
+        glVertexAttribPointer(aPositionLocation, 2, GL_FLOAT, false, 0, vertexData)
+        glEnableVertexAttribArray(aPositionLocation)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        glClear(GL_COLOR_BUFFER_BIT)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+    }
+}
