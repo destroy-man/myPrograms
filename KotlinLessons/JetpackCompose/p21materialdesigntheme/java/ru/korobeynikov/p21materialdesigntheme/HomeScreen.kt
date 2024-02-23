0acb2617cd5293820072555cdef158d07040d0c1
destroy-man <73271630+destroy-man@users.noreply.github.com>
2024-02-23 13:02:43 +0500

    Add files via upload

---
+++ KotlinLessons/JetpackCompose/p21materialdesigntheme/java/ru/korobeynikov/p21materialdesigntheme/HomeScreen.kt
@@ -0,0 +1,10 @@
+package ru.korobeynikov.p21materialdesigntheme
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() = Button(onClick = {}) {
+    Text(text = "Some text")
+}
