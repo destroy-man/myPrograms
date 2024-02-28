35047c871af4769dbc4e5f527a34f9ceed67a5c9
destroy-man <73271630+destroy-man@users.noreply.github.com>
2024-02-28 17:52:23 +0500

    Add files via upload

---
+++ KotlinLessons/JetpackCompose/p31animationspec/java/ru/korobeynikov/p31animationspec/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.korobeynikov.p31animationspec
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
