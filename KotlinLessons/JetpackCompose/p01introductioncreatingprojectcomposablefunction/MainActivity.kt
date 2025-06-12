338e390babc81a943e35ddfb88f3d7b640feee05
destroy-man <73271630+destroy-man@users.noreply.github.com>
2025-06-12 12:12:50 +0500

    Add files via upload

---
+++ KotlinLessons/JetpackCompose/p01introductioncreatingprojectcomposablefunction/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.korobeynikov.p01introductioncreatingprojectcomposablefunction
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+private fun HelloText() {
+    Text("Hello Compose World!")
+}
