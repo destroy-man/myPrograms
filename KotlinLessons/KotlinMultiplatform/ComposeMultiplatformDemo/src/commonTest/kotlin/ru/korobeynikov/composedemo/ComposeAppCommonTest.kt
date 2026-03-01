f32f565aa87e54788af805ad48625f0bf534dcc5
destroy-man <73271630+destroy-man@users.noreply.github.com>
2026-03-01 12:50:39 +0500

    Add files via upload

---
+++ KotlinLessons/KotlinMultiplatform/ComposeMultiplatformDemo/src/commonTest/kotlin/ru/korobeynikov/composedemo/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package ru.korobeynikov.composedemo
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
